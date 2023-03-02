e6e5a7409472344c8b5602f54637ebcd532843d4
Lauri Harjukoski <lauriharjukoski@Lauris-MacBook-Air.local>
2023-03-02 17:27:39 +0200

    custom health check

---
+++ health_check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if curl https://wild-silence-1283.fly.dev/health | grep ok
+then
+    echo "ok working ok"
+    exit 0
+else
+    echo "ok not ok"
+    exit 1
+fi
+
+ #exit  exit status 1 means that the script "fails"
